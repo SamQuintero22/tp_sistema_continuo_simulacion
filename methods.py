56dae1397da067e0899c124986156ea173968144
agusc546 <aguscesari14@gmail.com>
2026-05-29 12:32:47 -0300

    updating definition the ec_dif

--- methods.py
+++ methods.py
@@ -6,10 +6,11 @@ def euler(qt, h, derivada):
 
 # Implementacion de la derivada
 def ec_dif(t, h_nivel, hc, K1, K2, g, A):
+  f_entrada = 15 + 5 * math.cos(0.1 * t)
   if h_nivel <= hc:
-
-    return ((1/A) * (15 + 5 * math.cos(0.1 * t))) - ((1/A) * (K1 * math.sqrt(g * h_nivel)))
+    f_salida = K1 * math.sqrt(g * h_nivel)
   else:
+    f_salida = (K1 + K2) * math.sqrt(g * h_nivel)
 
-    return ((1/A) * (15 + 5 * math.cos(0.1 * t))) - ((1/A) * (K1 + K2) * math.sqrt(g * h_nivel))
+  return (1/A) * f_entrada - (1/A) * f_salida
 
