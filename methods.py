98b943467f0375e3c73d0abb3b105ff2899dede3
SamQuintero22 <saamuel.quintero@gmail.com>
2026-05-29 16:02:48 -0300

    Definimos el metodo de Heun en methods.py

--- methods.py
+++ methods.py
@@ -1,16 +1,31 @@
 import math
 
 # Implementacion del metodo de Euler
-def euler(qt, h, derivada):
-  return qt + h * derivada ## y(t + h)
+def euler(altura_actual, paso_integracion, derivada):
+  return altura_actual + paso_integracion * derivada ## y(t + h)
 
 # Implementacion de la derivada
-def ec_dif(t, h_nivel, hc, K1, K2, g, A):
-  f_entrada = 15 + 5 * math.cos(0.1 * t)
-  if h_nivel <= hc:
-    f_salida = K1 * math.sqrt(g * h_nivel)
+def ec_dif(altura_actual, tiempo, altura_critica, K1, K2, gravedad, area):
+  f_entrada = 15 + 5 * math.cos(0.1 * tiempo)
+  if altura_actual <= altura_critica:
+    f_salida = K1 * math.sqrt(gravedad * altura_actual)
   else:
-    f_salida = (K1 + K2) * math.sqrt(g * h_nivel)
+    f_salida = (K1 + K2) * math.sqrt(gravedad * altura_actual)
 
-  return (1/A) * f_entrada - (1/A) * f_salida
+  return (1/area) * f_entrada - (1/area) * f_salida
+
+# Implementacion del metodo Heun
+def heun(altura_actual, tiempo, altura_critica, K1, K2, gravedad, area, paso_integracion):
+    
+    # primer derivada
+    k1 = ec_dif(altura_actual, tiempo, altura_critica, K1, K2, gravedad, area)
+
+    # calculo auxiliar Euler (Xk + h*k1)
+    altura_euler = altura_actual + paso_integracion * k1
+
+    # segunda derivada, usando la estimacion 
+    k2 = ec_dif(tiempo + paso_integracion, altura_euler, altura_critica, K1, K2, gravedad, area)
+    
+    # promedio de las dos derivadas
+    return altura_actual + 0.5 * paso_integracion * (k1 + k2)
 
